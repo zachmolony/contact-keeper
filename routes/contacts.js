17b867bd98887b9d288042959876d5dfba6cdeb2
zachmolony <31472522+zachmolony@users.noreply.github.com>
2020-04-14 18:25:44 +0100

    added delete contact method

--- routes/contacts.js
+++ routes/contacts.js
@@ -104,11 +104,26 @@ router.put('/:id', auth, async (req, res) => {
 	}
 });
 
-// @route   GET api/contacts/:id
-// @desc    Delete contact
-// @access  Private
-router.delete('/',  (req, res) => {
-    res.send('Delete contact')
+// @route    DELETE api/contacts/:id
+// @desc     Delete a contact
+// @access   Private
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		const contact = await Contact.findById(req.params.id);
+
+		if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+		// Make sure user owns contact
+		if (contact.user.toString() !== req.user.id)
+			return res.status(401).json({ msg: 'Not authorized' });
+
+		await Contact.findByIdAndRemove(req.params.id);
+
+		res.json({ msg: 'Contact removed' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
 });
 
 module.exports = router;
