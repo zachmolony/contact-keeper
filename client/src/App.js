b21c64c9a99a6851885b5c46af4ed31b90e343ff
zachmolony <31472522+zachmolony@users.noreply.github.com>
2020-04-15 14:41:12 +0100

    created initial pages and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">My app</div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
